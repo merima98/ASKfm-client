d67efd61493c8e0f7b7cbe2ecfd87e6ce65bcfed
merima98 <merimaceranic98@gmail.com>
2021-02-22 17:49:16 +0100

    Adding Home screen

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import Home from "./features/home/Home";
+import Login from "./features/login/Login";
+import Signup from "./features/signup/Signup";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/features/home/Home.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+import { BREAKPOINTS } from "../../constants";
+
+const Wrapper = styled.div`
+  padding-top: 12rem;
+  text-align: center;
+`;
+const Form = styled.div`
+  display: grid;
+  margin: 0 auto;
+  width: 70%;
+  grid-template-rows: repeat(2, 1fr);
+
+  @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
+    width: 40%;
+  }
+`;
+const Logo = styled.div`
+  font-size: 20px;
+  color: white;
+  font-weight: bold;
+  margin-bottom: 2rem;
+`;
+const Caption = styled.div`
+  color: white;
+  margin-bottom: 2rem;
+`;
+const LoginButton = styled(NavLink)`
+  margin-bottom: 1rem;
+  height: 30px;
+  text-decoration: none;
+  border: 1px solid #fff;
+  border-radius: 4px;
+  color: #fff;
+  padding: 4px;
+`;
+
+const SignupButton = styled(NavLink)`
+  margin-bottom: 1rem;
+  height: 30px;
+  text-decoration: none;
+  background-color: white;
+  border-radius: 4px;
+  color: #2d3238;
+  padding: 4px;
+`;
+function Home() {
+  return (
+    <Wrapper>
+      <Logo>ASK.me</Logo>
+      <Caption>Curious? Just ask! Openly or anonymously.</Caption>
+      <Form>
+        <SignupButton exact to="/signup">
+          Sign up
+        </SignupButton>
+        <LoginButton exact to="/login">
+          Log in
+        </LoginButton>
+      </Form>
+    </Wrapper>
+  );
+}
+
+export default Home;
